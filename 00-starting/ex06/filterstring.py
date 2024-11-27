054b21f1ee2c73196b280757eba54adc3444f465
Seojun Yoo <honeybeei@Seojuns-MacBook-Pro.local>
2024-11-27 17:49:23 +0100

    ex06 finished

--- 00-starting/ex06/filterstring.py
+++ 00-starting/ex06/filterstring.py
@@ -1,13 +1,23 @@
 from sys import argv
+from ft_filter import ft_filter
 
 
 def main():
     """This is the main function"""
     try:
         args = argv
+        # Check arguments
         if not len(args) == 3:
             raise AssertionError("Please provide two argument")
-        print(args)
+        if not args[2].isdigit():
+            raise AssertionError("Second argument should be a positive int")
+        input_string = args[1]
+        input_integer = int(args[2])
+        splitted_words = input_string.split()
+        filtered_result = ft_filter(
+            lambda word: len(word) > input_integer, splitted_words
+        )
+        print(filtered_result)
     except AssertionError as e:
         print(f"AssertionError: {e}")
         exit(1)
